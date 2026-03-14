f6a9cfb9bf1bcd731bcba3254a8417aca69afba0
parambrotodutta-star <parambrotodutta@gmail.com>
2026-03-14 19:19:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,74 +1,34 @@
-body{
-font-family:Arial;
-margin:0;
-background:#f5f5f5;
-transition:0.3s;
+// Search filter
+
+const search = document.getElementById("search");
+const cards = document.querySelectorAll(".card");
+
+search.addEventListener("keyup", function(){
+
+let value = search.value.toLowerCase();
+
+cards.forEach(card =>{
+
+let text = card.textContent.toLowerCase();
+
+if(text.includes(value)){
+card.style.display="block";
+}
+else{
+card.style.display="none";
 }
 
-header{
-display:flex;
-justify-content:center;
-align-items:center;
-gap:15px;
-padding:20px;
-background:#111;
-color:white;
-flex-wrap:wrap;
-}
+});
 
-#search{
-padding:10px;
-border-radius:5px;
-border:none;
-}
+});
 
-#darkMode{
-padding:10px;
-cursor:pointer;
-border:none;
-border-radius:5px;
-}
 
-.products{
-display:grid;
-grid-template-columns:repeat(auto-fit,minmax(220px,1fr));
-gap:20px;
-padding:30px;
-}
+// Dark mode
 
-.card{
-background:white;
-padding:15px;
-border-radius:10px;
-text-align:center;
-box-shadow:0 5px 10px rgba(0,0,0,0.1);
-transition:0.3s;
-}
+const darkBtn = document.getElementById("darkMode");
 
-.card:hover{
-transform:scale(1.05);
-}
+darkBtn.onclick = function(){
 
-.card img{
-width:100%;
-border-radius:5px;
-}
+document.body.classList.toggle("dark");
 
-.card a{
-display:inline-block;
-margin-top:10px;
-background:#ff6600;
-color:white;
-padding:8px 15px;
-text-decoration:none;
-border-radius:5px;
-}
-
-.dark{
-background:#1e1e1e;
-color:white;
-}
-
-.dark .card{
-background:#333;
-}
+};
